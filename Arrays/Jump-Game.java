ffdc46b7169cf72fd143ea7e26db8a22b941b476
Ankush Tripathi <89534251+ankushtripathii@users.noreply.github.com>
2023-10-03 02:13:18 +0530

    Create Jump-Game.java
    leetcode problem 55. Jump Game

---
+++ Arrays/Jump-Game.java
@@ -0,0 +1,18 @@
+// leetcode problem 55. Jump Game
+// Medium
+
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reach=0;
+        for(int i=0;i<nums.length;i++){
+            if(reach<i){
+                return false;
+            }
+            reach=Math.max(reach,i+nums[i]);
+        }
+        return true;
+
+
+    }
+}
